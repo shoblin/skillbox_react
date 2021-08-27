3472a4546a217fc783714ed800541432565a8f33
shoblin <32629393+shoblin@users.noreply.github.com>
2021-08-27 20:08:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
+        <h2>Terraform</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> 
